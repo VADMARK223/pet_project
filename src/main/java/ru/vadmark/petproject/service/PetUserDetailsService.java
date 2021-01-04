30215413ac6a865b24c2d4c8f5258f534e708b80
VADMARK <random223@mail.ru>
2021-01-04 19:32:36 +0300

    User details service.

--- src/main/java/ru/vadmark/petproject/service/PetUserDetailsService.java
+++ src/main/java/ru/vadmark/petproject/service/PetUserDetailsService.java
@@ -1,8 +1,34 @@
 package ru.vadmark.petproject.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.vadmark.petproject.entity.UserEntity;
+import ru.vadmark.petproject.repository.UserEntityRepository;
+
 /**
  * Author: Markitanov Vadim
  * Date: 04.01.2021
  */
-public class PetUserDetailsService {
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PetUserDetailsService implements UserDetailsService {
+    private final UserEntityRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Username: {}.", username);
+
+        UserEntity user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return PetUserDetails.fromUserEntity(user);
+    }
 }
--- src/main/java/ru/vadmark/petproject/service/UserService.java
+++ src/main/java/ru/vadmark/petproject/service/UserService.java
@@ -5,9 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.vadmark.petproject.entity.RoleEntity;
@@ -23,24 +20,11 @@ import ru.vadmark.petproject.repository.UserEntityRepository;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService {
     public static final String ROLE_USER = "ROLE_USER";
     private final UserEntityRepository userRepository;
     private final RoleEntityRepository roleEntityRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        log.info("Username: {}.", username);
-
-        UserEntity user = userRepository.findByUsername(username);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-
-        return PetUserDetails.fromUserEntity(user);
-    }
 
     public boolean saveUser(UserForm userForm) {
         if (userRepository.findByUsername(userForm.getName()) != null) {
