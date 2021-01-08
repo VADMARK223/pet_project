1c1c1961a8eba9d290fdc4c27c31b1b71ec7ff17
VADMARK223 <vadmark223@mail.ru>
2021-01-08 22:44:33 +0300

    Admin

---
+++ svelte/src/app/pages/AdminPage.svelte
@@ -0,0 +1,15 @@
+<script>
+    let data = "data";
+
+    async function fetchDataHandler() {
+        const url = "http://localhost:8080/svelte/users";
+        const response = await fetch(url);
+
+        data = await response.text();
+        document.getElementById("temp").insertAdjacentHTML('afterend', data)
+    }
+</script>
+
+<button on:click={fetchDataHandler}>Fetch users</button>
+<hr>
+<div id="temp"></div>
