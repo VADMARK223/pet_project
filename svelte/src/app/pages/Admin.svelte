724b884de7b839380efc1f605461e317190a6b89
VADMARK223 <vadmark223@mail.ru>
2021-01-09 12:44:03 +0300

    Renaming

--- svelte/src/app/pages/Admin.svelte
+++ svelte/src/app/pages/Admin.svelte
@@ -3,7 +3,6 @@
     let users = "";
 
     async function fetchDataHandler() {
-        console.log(">>>>   " + process.env.API_BASE_URL);
         const url = process.env.API_BASE_URL + "/admin";
         const response = await fetch(url);
         users = await response.json();
--- svelte/src/app/routing/Router.svelte
+++ svelte/src/app/routing/Router.svelte
@@ -1,13 +1,13 @@
 <script>
     import {hash} from '../services/router';
-    import HomePage from '../pages/HomePage.svelte';
-    import LoginPage from '../pages/LoginPage.svelte';
-    import LogoutPage from '../pages/LogoutPage.svelte';
-    import RegistrationPage from '../pages/RegistrationPage.svelte';
-    import AdminPage from '../pages/AdminPage.svelte';
-    import SettingsPage from '../pages/SettingsPage.svelte';
-    import TestPage from '../pages/TestPage.svelte';
-    import NotFoundPage from '../pages/NotFoundPage.svelte';
+    import HomePage from '../pages/Home.svelte';
+    import LoginPage from '../pages/Login.svelte';
+    import LogoutPage from '../pages/Logout.svelte';
+    import RegistrationPage from '../pages/Registration.svelte';
+    import AdminPage from '../pages/Admin.svelte';
+    import SettingsPage from '../pages/Settings.svelte';
+    import TestPage from '../pages/Test.svelte';
+    import NotFoundPage from '../pages/NotFound.svelte';
 
     let value = HomePage;
 
