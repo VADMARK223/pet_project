09f4fc57a17d5569f86e5da8ccd4c54e9da81d16
v.markitanov <vadmark223@mail.ru>
2021-01-26 00:25:42 +0300

    Not redirect for client.

--- src/main/java/ru/vadmark/petproject/handler/CustomRedirectStrategy.java
+++ src/main/java/ru/vadmark/petproject/handler/CustomRedirectStrategy.java
@@ -1,10 +1,27 @@
 package ru.vadmark.petproject.handler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.web.DefaultRedirectStrategy;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author Markitanov Vadim
  * @since 25.01.2021
  */
+@Slf4j
 public class CustomRedirectStrategy extends DefaultRedirectStrategy {
+
+    @Override
+    public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
+        final String origin = request.getHeader(HttpHeaders.ORIGIN);
+        if ("http://localhost:5000".equals(origin)) {
+            log.info("Not redirect for client.");
+            return;
+        }
+        super.sendRedirect(request, response, url);
+    }
 }
