e3ea938b6eaa7aeee8cff626d122aa38ffdd394a
v.markitanov <vadmark223@mail.ru>
2021-02-01 23:50:54 +0300

    Svelte: empty modal for add record weight.

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -25,8 +25,26 @@
     })
 </script>
 
+<div class="modal fade show" tabindex="-1" id="recordModal" aria-modal="true" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Add weight</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <p>Empty body</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <header>
     <h2><a href="/">Svelte</a></h2>
+    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#recordModal">Add weight</button>
     {#if username}
         <h4>Welcome back, {username}.</h4>
     {/if}
@@ -47,5 +65,9 @@
     article {
         height: 100%;
         overflow: hidden;
+    }
+
+    .modal {
+        display: none;
     }
 </style>
